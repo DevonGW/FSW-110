b576c958fc582004523c5e0f3f0e7d5c54c1c3ac
DevonGW <>
2020-06-28 20:44:08 -0700

    pause and practice week 2

---
+++ week2/Week2PP.js
@@ -0,0 +1,24 @@
+let newH1 = document.createElement("h1");
+newH1.textContent = "Welcome To My JS Site"
+let header = document.getElementById("h1") ;
+header.append(newH1)
+
+let newP = document.createElement("p")
+newP.textContent = "Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+document.body.append(newP)
+
+let newLi = document.createElement("li");
+newLi.textContent = "headphones, radio, water, food, shoes"
+let myList = document.getElementById("h1") ;
+myList.append(newLi)
+
+
+
+
+// Create a new html and js file, then link them together.
+// In the js file, create the following elements and append them to the DOM so they show up when the website is visited.
+// H1 with the text “Welcome to my JS site”
+// P wit the text “All of this was created with Javascript”
+// An Ordered list (ol) with at least three items (li).
+// Each li should also display text.
+// Remember, you will need to first add the li’s to the ol before putting the ol on the DOM.
