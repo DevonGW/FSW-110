01567951fb4e2fc69e5af948779f0396d9df4715
DevonGW <>
2020-07-22 17:01:17 -0500

    capstone app

---
+++ week4/Week4PP.js
@@ -0,0 +1,36 @@
+
+
+document.getElementById("box").style.border = "thick solid black"
+document.getElementById("box").style.padding = "100px 100px 100px 100px";
+document.getElementById("box").style.margin = "25% 90% 2% 80%";
+document.getElementById("box").style.marginLeft = "0%"
+
+
+
+document.getElementById("box2").style.border = "thick solid black"
+document.getElementById("box2").style.padding = "100px 100px 100px 100px";
+document.getElementById("box2").style.margin = "0% 50% 2% 50%";
+
+
+
+document.getElementById("box3").style.border = "thick solid black"
+document.getElementById("box3").style.padding = "100px 100px 100px 100px";
+document.getElementById("box3").style.margin = "0% 50% 2% 50%";
+
+
+document.getElementById("box4").style.border = "thick solid black"
+document.getElementById("box4").style.padding = "100px 100px 100px 100px";
+document.getElementById("box4").style.margin = "0% 50% 2% 50%";
+
+
+
+
+
+
+// The square's color will change as follows:
+
+// Blue when the mouse hovers over the square
+// Red when the mouse button is held down over the square
+// Yellow when the mouse button is let go over the square
+// Green when the mouse is double clicked in the square
+// Orange when the mouse scroll is used somewhere in the window (not just over the square).
